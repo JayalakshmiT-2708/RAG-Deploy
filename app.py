714b371ddd1ff6b67da5e7281cd539f0d7731558
JayalakshmiT-2708 <jayalakshmikishore27@gmail.com>
2026-05-20 14:21:19 +0530

    Change app run configuration to use environment port

--- app.py
+++ app.py
@@ -128,4 +128,5 @@ Your goal is to ensure 100% factual grounding in the given document."""
 
 #run app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
